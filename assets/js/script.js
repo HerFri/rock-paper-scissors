1f22736250ab36b42ea70e06c2a0d92e1d97c480
Her Fri <herbert.frichert@outlook.de>
2023-07-26 00:40:03 +0200

    add showImages onClick functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+/* var choices = ["rock", "paper", "scissors"] */
+
+/*document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("input");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.get)
+        }
+         )
+    }
+
+}) */
+
+
+
 function runGame() {
 
 }
@@ -12,4 +28,26 @@ function incrementScore() {
 
 function incrementStreak() {
     
+}
+
+function selectChoice() {
+
+}
+
+function showRock() {
+  document.getElementById("first").style.display ='block';
+  document.getElementById("second").style.display ='none';
+  document.getElementById("third").style.display ='none';
+}
+
+function showPaper() {
+    document.getElementById("first").style.display ='none';
+    document.getElementById("second").style.display ='block';
+    document.getElementById("third").style.display ='none';
+}
+
+function showScissors() {
+    document.getElementById("first").style.display ='none';
+    document.getElementById("second").style.display ='none';
+    document.getElementById("third").style.display ='block';
 }
