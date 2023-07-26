7485645f82d8c25554a653a734382f7d38b48821
Her Fri <herbert.frichert@outlook.de>
2023-07-26 19:12:05 +0200

    add JavaScript for images to show when computer made chpice, add some js sketches

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,65 @@
 
 }) */
 
-
-
-function runGame() {
-
+function generateNumber() {
+    let genNbr = Math.floor(Math.random() * 3) +1;
 }
+
+/*function runGame() {
+    let rockBtn = getElementById("rock");
+    let paperBtn = getElementById("paper");
+    let scissorsBtn = getElementById("scissors");
+    let playerOptions = [rockBtn,paperBtn,scissorsBtn];
+    let computerOptions = ["rock","paper","scissors"];
+
+    playerOptions.forEach(option => {
+        option.addEventListener("click", function(){
+
+            let choiceNumber = Math.floor(Math.random() * 3) +1;
+            let ComputerChoice = computerOptions[choiceNumber];
+            if (choiceNumber === 1){
+                showRockComp()
+            } else if (choiceNumber === 2){
+                showPaperComp()
+            } else if (choiceNumber === 3){
+                showScissorsComp()
+            }
+    })
+    }
+    )
+    } */
+
+function playGame() {
+    document.getElementsByClassName("images").onclick = function() {
+        
+
+        let choiceNumber = Math.floor(Math.random() * 3) +1;
+            if (choiceNumber === 1){
+                showRockComp();
+            } else if (choiceNumber === 2){
+                showPaperComp();
+            } else if (choiceNumber === 3){
+                showScissorsComp();
+            }
+        console.log(choiceNumber)
+    }
+}
+    
+            
+            
+            /*console.log(ComputerNumber) */
+
+
+
+
+
+
+   /* document.getElementById("rock").addEventListener("click", function() {
+
+    } */
+    
+    
+
 
 function checkAnswer() {
 
@@ -51,3 +105,21 @@ function showScissors() {
     document.getElementById("second").style.display ='none';
     document.getElementById("third").style.display ='block';
 }
+
+function showRockComp() {
+    document.getElementById("fourth").style.display ='block';
+    document.getElementById("fifth").style.display ='none';
+    document.getElementById("sixth").style.display ='none';
+  }
+  
+  function showPaperComp() {
+      document.getElementById("fourth").style.display ='none';
+      document.getElementById("fifth").style.display ='block';
+      document.getElementById("sixth").style.display ='none';
+  }
+  
+  function showScissorsComp() {
+      document.getElementById("fourth").style.display ='none';
+      document.getElementById("fifth").style.display ='none';
+      document.getElementById("sixth").style.display ='block';
+  }
